65f714bc5435617ebe0f4baab4a71341cf480943
Rishikesh Vishwakarma <rishikesh@tupronto.mx>
2020-03-06 14:53:39 +0530

    Add Link list and redirect

--- client/main.js
+++ client/main.js
@@ -5,12 +5,14 @@ import { render } from 'react-dom'
 import Header from '../imports/components/header'
 import LinkCreate from '../imports/components/link_create'
 import Links from '../imports/collections/links'
+import LinkList from '../imports/components/link_list'
 
 const App = () => {
   return (
     <div>
       <Header />
       <LinkCreate />
+      <LinkList />
     </div>
   )
 }
